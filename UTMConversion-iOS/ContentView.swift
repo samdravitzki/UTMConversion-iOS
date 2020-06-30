6f63aa704efbe797668d553cac185620a2511f05
Sam Dravitzki <srd62@uclive.ac.nz>
2020-06-30 17:39:35 +1200

    Intergrated the map view into the main content view and added data output

--- UTMConversion-iOS/ContentView.swift
+++ UTMConversion-iOS/ContentView.swift
@@ -7,10 +7,51 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    
+    
     var body: some View {
-        Text("Hello, World!")
+
+        
+        VStack {
+            ZStack {
+                MapView(centerCoordinate: $centerCoordinate)
+                Circle()
+                    .fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+            }
+            
+            VStack {
+                Text("WGS48")
+                HStack {
+                    Text("Coordinates")
+                    Text("\(centerCoordinate.latitude), \(centerCoordinate.longitude)")
+                    .frame(width: 200)
+                }
+                .padding(.bottom)
+                
+                Text("UTM")
+                
+                HStack {
+                    Text("Zone")
+                    Text("Unknown")
+                    .frame(width: 200)
+                }
+                HStack {
+                    Text("Coordinates")
+                    Text("Unknown, Unknown")
+                    .frame(width: 200)
+                }
+            }
+        .frame(height: 200)
+            
+        
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
