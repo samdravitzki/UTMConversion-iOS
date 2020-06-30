c9b5525d1d8cc18225280d0baae99975c2ad96a9
Sam Dravitzki <srd62@uclive.ac.nz>
2020-06-30 17:25:21 +1200

    Initial Commit

---
+++ UTMConversion-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UTMConversion-iOS
+//
+//  Created by Sam Dravizki on 30/06/20.
+//  Copyright © 2020 Sam Dravizki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
